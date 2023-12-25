2cbda9df204c80bd3e57066294f71ae4cfc7913d
RimDallali <dallali.rim1@gmail.com>
2023-12-25 00:24:34 -0500

    work on background color and scroll bar

--- cra-threejs/src/components/home/Three.js
+++ cra-threejs/src/components/home/Three.js
@@ -7,9 +7,10 @@ function MyThree() {
     useEffect(() => {
         // === THREE.JS CODE START ===
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor(0x1e222f, 1);
         // document.body.appendChild( renderer.domElement );
         // use ref as a mount point of the Three.js scene instead of the document.body
         refContainer.current && refContainer.current.appendChild(renderer.domElement);
