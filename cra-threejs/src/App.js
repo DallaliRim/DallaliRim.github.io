f3bade5afd9b2be6deaf1c2ba6f795a62a1eee8a
RimDallali <dallali.rim1@gmail.com>
2023-12-24 22:14:59 -0500

    create react app with 3js to deploy

---
+++ cra-threejs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyThree from './Three';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyThree />
+      </header>
+    </div>
+  );
+}
+
+export default App;
